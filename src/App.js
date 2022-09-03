8d68725dde70a4a6a18e7eaa039cdf872538ed88
Daniel Kim <danielkim2711@gmail.com>
2022-09-03 12:32:13 +1200

    Create navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 
 const App = () => (
   <Routes>
-    <Route path='/' element={<Home />} />
+    <Route path='/' element={<Navigation />}>
+      <Route index element={<Home />} />
+    </Route>
   </Routes>
 );
 
