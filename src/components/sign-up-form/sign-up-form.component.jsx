53d06a81475cbada50db36a0e881589114e97333
Daniel Kim <danielkim2711@gmail.com>
2022-09-09 23:51:26 +1200

    Sign in with email and password

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
+
 const defaultFormFields = {
   displayName: '',
   email: '',
@@ -17,14 +22,30 @@ const SignUpForm = () => {
     setFormFields({ ...formFields, [name]: value });
   };
 
-  const onSubmitHandler = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    try {
+      const response = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserDocumentFromAuth(response.user, { displayName });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div>
       <h1>Sign up with your email and password</h1>
-      <form onSubmit={onSubmitHandler}>
+      <form onSubmit={handleSubmit}>
         <label htmlFor=''>Display Name</label>
         <input
           type='text'
