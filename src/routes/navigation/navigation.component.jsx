580bd19d1922a8e07939271b61dfe8eec7194e88
Daniel Kim <danielkim2711@gmail.com>
2022-09-15 15:56:53 +1200

    Create cart context and toggle dropdown menu

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,17 +2,19 @@ import { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 import { UserContext } from '../../contexts/user.context';
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { CartContext } from '../../contexts/cart.context';
 
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <>
@@ -36,7 +38,7 @@ const Navigation = () => {
           )}
           <CartIcon />
         </div>
-        <CartDropdown />
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </>
