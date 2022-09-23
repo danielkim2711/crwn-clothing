6ac5d5c4e710545027caf0d506b57c0db77a8b93
Daniel Kim <danielkim2711@gmail.com>
2022-09-23 15:09:04 +1200

    Show categories documents data on shop page

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -2,28 +2,29 @@ import { createContext, useState, useEffect } from 'react';
 
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
-export const ProductsContext = createContext({
-  products: [],
-  setProducts: () => null,
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+  setCategoriesMap: () => {},
 });
 
-export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
 
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
       console.log(categoryMap);
+      setCategoriesMap(categoryMap);
     };
 
     getCategoriesMap();
   }, []);
 
-  const value = { products };
+  const value = { categoriesMap };
 
   return (
-    <ProductsContext.Provider value={value}>
+    <CategoriesContext.Provider value={value}>
       {children}
-    </ProductsContext.Provider>
+    </CategoriesContext.Provider>
   );
 };
